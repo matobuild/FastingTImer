00189c559983fb855bb4be931f7e0e3873e3ff63
Mato <tissuepaperarelimited@gmail.com>
2022-08-28 10:10:51 +0800

    Initial Commit

---
+++ FastingTImer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastingTImer
+//
+//  Created by kittawat phuangsombat on 2022/8/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastingTImer/FastingTImerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTImerApp.swift
+//  FastingTImer
+//
+//  Created by kittawat phuangsombat on 2022/8/28.
+//
+
+import SwiftUI
+
+@main
+struct FastingTImerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
